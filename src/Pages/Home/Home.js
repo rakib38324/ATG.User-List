02b03768cd285629cbc7a105727d68677df0bfd7
rakib38324 <rakib3832@gmail.com>
2023-02-22 19:43:45 +0600

    Setup the project with reactJs, React Router Dom, tailwindCSS

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Loading from '../Loading/Loading';
+
+const Home = () => {
+    return (
+        <div>
+            <p>Home Page</p>
+            <Loading></Loading>
+        </div>
+    );
+};
+
+export default Home;
